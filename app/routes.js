2a70e13a18280badfc7aeeb06ec903b9dba31b3b
briananddigital <104130473+brianMilmanConduent@users.noreply.github.com>
2022-06-01 13:41:39 +0100

    Ver8 created

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const v7Routes = require('./routes/v7');
+const v8Routes = require('./routes/v8');
+
 
 router.use('/', v7Routes);
+router.use('/', v8Routes);
+
 
 module.exports = router;
