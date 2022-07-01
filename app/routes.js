884605260edba715f29b57b689b6696afa169682
briananddigital <104130473+brianMilmanConduent@users.noreply.github.com>
2022-07-01 14:46:51 +0100

    Ver 10 created for UR and design iteration

--- app/routes.js
+++ app/routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const v7Routes = require('./routes/v7');
 const v8Routes = require('./routes/v8');
 const v9Routes = require('./routes/v9');
+const v10Routes = require('./routes/v10');
 
 router.use('/', v7Routes);
 router.use('/', v8Routes);
 router.use('/', v9Routes);
+router.use('/', v10Routes);
 
 module.exports = router;
