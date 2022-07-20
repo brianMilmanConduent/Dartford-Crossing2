07df0881b9576837c0e0678e5d7b2427871d954b
briananddigital <104130473+brianMilmanConduent@users.noreply.github.com>
2022-07-20 08:37:06 +0100

    ver12 created for Sprint 26

--- app/routes.js
+++ app/routes.js
@@ -6,11 +6,13 @@ const v8Routes = require('./routes/v8');
 const v9Routes = require('./routes/v9');
 const v10Routes = require('./routes/v10');
 const v11Routes = require('./routes/v11');
+const v12Routes = require('./routes/v12');
 
 router.use('/', v7Routes);
 router.use('/', v8Routes);
 router.use('/', v9Routes);
 router.use('/', v10Routes);
 router.use('/', v11Routes);
+router.use('/', v12Routes);
 
 module.exports = router;
